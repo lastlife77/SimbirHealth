4f9646c9b800f22bee98f9c7be96b7a98bf831e5
lastlife77 <iliya090202@gmail.com>
2024-10-22 21:23:10 +0300

    Микросервис Документов: создание основных сущностей

---
+++ DocumentService/Document.Domain/History.cs
@@ -0,0 +1,19 @@
+﻿namespace Document.Domain;
+
+public class History
+{
+    public int Id { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public int PatientId { get; set; }
+
+    public int HospitalId { get; set; }
+
+    public int DoctorId { get; set; }
+
+    public int RoomId { get; set; }
+
+    public string Data { get; set; }
+
+}
