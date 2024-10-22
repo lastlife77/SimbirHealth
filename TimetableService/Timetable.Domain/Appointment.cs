bc9fc0e4670cf2ee382384286b42510c9b315835
lastlife77 <iliya090202@gmail.com>
2024-10-22 21:25:08 +0300

    Микросервис Расписания: создание осноных сущностей

---
+++ TimetableService/Timetable.Domain/Appointment.cs
@@ -0,0 +1,12 @@
+﻿namespace TimeTable.Domain;
+
+public class Appointment
+{
+    public int Id { get; set; }
+
+    public DateTime Time { get; set; }
+
+    public int TimeTableId { get; set; }
+
+    public int? UserId { get; set; }
+}
---
+++ TimetableService/Timetable.Domain/Timetable.cs
@@ -0,0 +1,16 @@
+﻿namespace Timetable.Domain;
+
+public class Timetable
+{
+    public int Id { get; set; }
+
+    public int HospitalId { get; set; }
+
+    public int DoctorId { get; set; }
+
+    public DateTime From { get; set; }
+
+    public DateTime To { get; set; }
+
+    public int RoomId { get; set; }
+}
